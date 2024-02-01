8b74b44b5bc66782e7ea7277c71a655d87a14af7
Vinicius Gouvea <vrgouvea@outlook.com>
2024-02-01 10:57:27 -0300

    opengraph test3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,23 @@
 import { darker, major } from '@/styles/fonts'
 import type { Metadata } from 'next'
+import Head from 'next/head'
 import './globals.css'
 
-export const metadata: Metadata = {
+type OGImage = string | URL
+
+type CustomMetadata = Metadata & {
+  openGraph: {
+    images: OGImage
+  }
+}
+
+export const metadata: CustomMetadata = {
   title: 'tailvinicss',
-  description: `Vinicius Gouvea's porfolio`,
+  description: `Vinicius Gouvea's portfolio`,
+  metadataBase: new URL('https://tailvinicss.vercel.app'),
+  openGraph: {
+    images: 'https://tailvinicss.vercel.app/opengraph-image.png',
+  },
 }
 
 export default function RootLayout({
@@ -14,6 +27,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="description" content={metadata.description || ''} />
+        <meta property="og:description" content={metadata.description || ''} />
+        <meta
+          property="og:url"
+          content={metadata.metadataBase?.toString() || ''}
+        />
+        {metadata.openGraph.images && (
+          <meta
+            property="og:image"
+            content={metadata.openGraph.images.toString()}
+          />
+        )}
+      </Head>
       <body className={`${darker.variable} ${major.variable}`}>{children}</body>
     </html>
   )
